7c0c93918c444a20c7e858dc3245cbc951a77cce
Allen Guo <guosonglin001@gmail.com>
2024-04-08 23:00:32 +0800

    wip

--- _demos/t4.sparse-checkout.sh
+++ _demos/t4.sparse-checkout.sh
@@ -2,7 +2,10 @@
 git clone -n --depth=1 --filter=tree:0 -b allen/develop git@github.com:gglin001/llvm-project.git
 
 pushd llvm-project
-git sparse-checkout set --no-cone /clang /cmake /libunwind /lld /llvm /mlir /CMakePresets.json
+git sparse-checkout set --no-cone \
+  /clang /cmake /libunwind /lld /llvm /mlir /CMakePresets.json \
+  /_demos
+
 # git sparse-checkout set --no-cone /cmake /llvm /mlir
 git sparse-checkout list
 git checkout
