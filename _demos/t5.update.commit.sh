6b2248e609a20d44846549d30adced34471f74b5
Allen Guo <guosonglin001@gmail.com>
2024-04-15 13:45:40 +0800

    wip

--- _demos/t5.update.commit.sh
+++ _demos/t5.update.commit.sh
@@ -1,6 +1,8 @@
-LLVM_SHA="9708d0900311503aa4685d6810d8caf0412e15d7"
+# LLVM_SHA="9708d0900311503aa4685d6810d8caf0412e15d7"
+LLVM_SHA="6f1e23b47d428d792866993ed26f4173d479d43d"
 
 # git fetch origin $LLVM_SHA
 git fetch shark $LLVM_SHA
 
-git checkout -b sync $LLVM_SHA
+# git checkout -b sync $LLVM_SHA
+git branch sync $LLVM_SHA
