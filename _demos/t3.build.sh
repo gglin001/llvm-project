665468b4475a9e960e3ede6434d8bae3899ced95
Allen Guo <guosonglin001@gmail.com>
2024-05-05 13:56:52 +0800

    wip

--- _demos/t3.build.sh
+++ _demos/t3.build.sh
@@ -9,3 +9,4 @@ popd
 # cmake --preset iree_llvm -S$PWD/llvm -B$PWD/build
 cmake --preset iree_llvm_allen -S$PWD/llvm -B$PWD/build
 cmake --build $PWD/build --target all
+cmake --build $PWD/build --target install
