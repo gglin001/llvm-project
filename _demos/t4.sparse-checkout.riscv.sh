3a7db377d0b906d44ba91ca347c5b4bb919ba61d
Allen Guo <guosonglin001@gmail.com>
2024-06-17 00:00:28 +0800

    wip

---
+++ _demos/t4.sparse-checkout.riscv.sh
@@ -0,0 +1,21 @@
+# git clone -n --depth=1 --filter=tree:0 -b main git@github.com:llvm/llvm-project.git
+git clone -n --depth=1 --filter=tree:0 -b allen/develop git@github.com:gglin001/llvm-project.git
+
+pushd llvm-project
+
+git sparse-checkout set --no-cone \
+  /cmake /llvm /mlir \
+  '!/llvm/test' '!/llvm/unittests' '!/llvm/docs' \
+  '!/mlir/test' '!/mlir/docs' \
+  /_demos /.vscode '/*.*'
+
+# llvm/lib/Target
+git sparse-checkout add --no-cone \
+  '!/llvm/lib/Target' '/llvm/lib/Target/*.*' \
+  '/llvm/lib/Target/RISCV'
+
+# git sparse-checkout set --no-cone /cmake /llvm /mlir
+git sparse-checkout list
+git checkout
+
+popd
