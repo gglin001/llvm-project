98f4331454cbe4dedfb6cfdbe92f129726bb3a06
Allen Guo <guosonglin001@gmail.com>
2024-06-17 13:31:56 +0800

    wip

--- _demos/t4.sparse-checkout.riscv.sh
+++ _demos/t4.sparse-checkout.riscv.sh
@@ -4,15 +4,15 @@ git clone -n --depth=1 --filter=tree:0 -b allen/develop git@github.com:gglin001/
 pushd llvm-project
 
 git sparse-checkout set --no-cone \
-  /cmake /llvm /mlir \
+  /cmake /llvm /mlir /lld \
   '!/llvm/test' '!/llvm/unittests' '!/llvm/docs' \
   '!/mlir/test' '!/mlir/unittests' '!/mlir/docs' \
   /_demos /.vscode '/*.*'
 
-# llvm/lib/Target
 git sparse-checkout add --no-cone \
+  '!/llvm/utils/gn' \
   '!/llvm/lib/Target' '/llvm/lib/Target/*.*' \
-  '/llvm/lib/Target/RISCV'
+  '/llvm/lib/Target/RISCV' '/llvm/lib/Target/ARM' '/llvm/lib/Target/AArch64'
 
 # git sparse-checkout set --no-cone /cmake /llvm /mlir
 git sparse-checkout list
