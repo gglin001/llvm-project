1b7cb8fbbd2e0c8351ab7cc0a0203775b381b168
Allen Guo <guosonglin001@gmail.com>
2024-09-20 12:04:02 +0800

    wip

--- _demos/t3.build.sh
+++ _demos/t3.build.sh
@@ -1,21 +1,17 @@
-# pushd llvm
-# ln -s ../CMakePresets.json .
-# ln -s ../CMakeUserPresets.json .
-# popd
+###############################################################################
 
-# building with cmake-tools in vscode
-
-# or
 # cmake --preset osx -S$PWD/llvm -B$PWD/build
 cmake --preset osx_allen -S$PWD/llvm -B$PWD/build
+
 cmake --build $PWD/build --target all
 cmake --build $PWD/build --target install
 
 ###############################################################################
 
-pushd build
-ln -s install Release
-popd
+# optional
+# pushd build
+# ln -s install Release
+# popd
 
 llvm-config --libs
 
